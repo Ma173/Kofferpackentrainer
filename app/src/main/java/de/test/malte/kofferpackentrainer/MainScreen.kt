ba4f12eaa113ba666ed4c6a2fdd88bfe55608b62
BuildTools <unconfigured@null.spigotmc.org>
2020-02-17 03:11:21 +0100

    Zwischenstand: neue Übung wird generiert und letztes Element(?) wird in editText "exercise" geschrieben
    Als nächstes: stattdessen listView verwenden und einzelne Elemente der Übung in listView schreiben

--- app/src/main/java/de/test/malte/kofferpackentrainer/MainScreen.kt
+++ app/src/main/java/de/test/malte/kofferpackentrainer/MainScreen.kt
@@ -11,10 +11,10 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main_screen.*
 import kotlinx.android.synthetic.main.app_bar_main_screen.*
 import kotlinx.android.synthetic.main.content_main_screen.*
-import java.lang.Math.random
 import java.util.Random
 
 //import kotlinx.android.synthetic.main.main_screen.*
@@ -22,6 +22,65 @@ import java.util.Random
 class MainScreen : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     //item changeUser
+
+    var elementeAusStand :  Map<String,Array<Any>> = mapOf(
+            "Hocke" to arrayOf(0, "Stand", "Stand", 0),
+            "Bücke" to arrayOf(0, "Stand", "Stand", 0),
+            "Grätsche" to arrayOf(0, "Stand", "Stand", 0),
+            "halbe Schraube" to arrayOf(0.1, "Stand", "Stand", 0),
+            "halbe Hocke" to arrayOf(0.1, "Stand", "Stand", 1),
+            "halbe Bücke" to arrayOf(0.1, "Stand", "Stand", 1),
+            "halbe Grätsche" to arrayOf(0.1, "Stand", "Stand", 1),
+            "ganze Schraube" to arrayOf(0.2, "Stand", "Stand", 0),
+            "Sitz" to arrayOf(0, "Stand", "Sitz", 0),
+            "halbe Sitz" to arrayOf(0.1, "Stand", "Sitz", 0),
+            "ganze Sitz" to arrayOf(0.2, "Stand", "Sitz", 1),
+            "Hocke in den Sitz" to arrayOf(0, "Stand", "Sitz", 2),
+            "Grätsche in den Sitz" to arrayOf(0, "Stand", "Sitz", 2),
+            "Rücken" to arrayOf(0.1,"Stand", "Rücken", 0),
+            "Halbe Rücken" to arrayOf(0.2, "Stand", "Rücken", 0),
+            "Bauch" to arrayOf(0.1, "Stand","Bauch", 0)
+    )
+    var elementeAusSitz :  Map<String,Array<Any>> = mapOf(
+            "Stand" to arrayOf(0, "Sitz", "Stand", 0),
+            "halbe Stand" to arrayOf(0.1, "Sitz", "Stand", 0),
+            "ganze Stand" to arrayOf(0.2, "Sitz", "Stand", 1),
+            "Sitz" to arrayOf(0, "Sitz", "Sitz", 0),
+            "halbe Sitz" to arrayOf(0.1, "Sitz", "Sitz", 0),
+            "ganze Sitz" to arrayOf(0.2, "Sitz", "Sitz", 0),
+            "Rücken" to arrayOf(0.1, "Sitz", "Rücken", 1),
+            "Halbe Rücken" to arrayOf(0.2, "Sitz", "Rücken", 1),
+            "Bauch" to arrayOf(0.1, "Sitz", "Bauch", 0)
+    )
+    var elementeAusRuecken :  Map<String,Array<Any>> = mapOf(
+            "Stand" to arrayOf(0.1, "Rücken", "Stand", 0),
+            "halbe Stand" to arrayOf(0.2, "Rücken", "Stand", 0),
+            "ganze Stand" to arrayOf(0.3, "Rücken", "Stand", 1),
+            "Muffel" to arrayOf(0.3, "Rücken", "Stand", 0),
+            "Salto vorwärts z. Stand" to arrayOf(0.6, "Rücken", "Stand", 1),
+            "halbe Sitz" to arrayOf(0.2, "Rücken", "Sitz", 1),
+            "Muffel in denSitz" to arrayOf(0.3, "Rücken", "Sitz", 0),
+            "Rücken" to arrayOf(0, "Rücken", "Rücken", 0),
+            "Halbe Rücken" to arrayOf(0.3, "Rücken", "Rücken", 0),
+            "Salto vorwärts z. Rücken c" to arrayOf(0.5, "Rücken", "Rücken", 0),
+            "Muffel in den Rücken" to arrayOf(0.5, "Rücken", "Stand", 1),
+            "Bauch" to arrayOf(0.2, "Rücken", "Bauch", 1),
+            "Muffel in den Bauch" to arrayOf(0.2, "Rücken", "Bauch", 1),
+            "Wende in den Bauch" to arrayOf(0.2, "Rücken", "Bauch", 0)
+    )
+    var elementeAusBauch :  Map<String,Array<Any>> = mapOf(
+            "Stand" to arrayOf(0.1, "Bauch", "Stand", 0),
+            "halbe Stand" to arrayOf(0.1, "Bauch", "Stand", 0),
+            "Rücken" to arrayOf(0.2, "Bauch", "Rücken", 0),
+            "Wende in den Rücken" to arrayOf(0.2, "Bauch", "Rücken", 0),
+            "Bauch" to arrayOf(0, "Bauch", "Bauch", 0),
+            "halbe Heli" to arrayOf(0.1, "Bauch", "Bauch", 0)
+    )
+
+    val random = Random()
+    fun rand(from: Int, to: Int): Int {
+        return random.nextInt(to - from) + from
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,76 +101,15 @@ class MainScreen : AppCompatActivity(), NavigationView.OnNavigationItemSelectedL
 
         //TODO: Hier den Import der Elemente-Textdatei in die Datenstruktur (z.B. HashMap) einbauen
         //TODO: Nachdem alle Elemente importiert sind, können die in den Usersettings angezeigt werden
-
-        var elementeAusStand :  Map<String,Array<Any>> = mapOf(
-                "Hocke" to arrayOf(0, "Stand", "Stand", 0),
-                "Bücke" to arrayOf(0, "Stand", "Stand", 0),
-                "Grätsche" to arrayOf(0, "Stand", "Stand", 0),
-                "halbe Schraube" to arrayOf(0.1, "Stand", "Stand", 0),
-                "halbe Hocke" to arrayOf(0.1, "Stand", "Stand", 1),
-                "halbe Bücke" to arrayOf(0.1, "Stand", "Stand", 1),
-                "halbe Grätsche" to arrayOf(0.1, "Stand", "Stand", 1),
-                "ganze Schraube" to arrayOf(0.2, "Stand", "Stand", 0),
-                "Sitz" to arrayOf(0, "Stand", "Sitz", 0),
-                "halbeSitz" to arrayOf(0.1, "Stand", "Sitz", 0),
-                "ganze Sitz" to arrayOf(0.2, "Stand", "Sitz", 1),
-                "Hocke in den Sitz" to arrayOf(0, "Stand", "Sitz", 2),
-                "Grätsche in den Sitz" to arrayOf(0, "Stand", "Sitz", 2),
-                "Rücken" to arrayOf(0.1,"Stand", "Rücken", 0),
-                "Halbe Rücken" to arrayOf(0.2, "Stand", "Rücken", 0),
-                "Bauch" to arrayOf(0.1, "Stand","Bauch", 0)
-        )
-        var elementeAusSitz :  Map<String,Array<Any>> = mapOf(
-                "Stand" to arrayOf(0, "Sitz", "Stand", 0),
-                "halbe Stand" to arrayOf(0.1, "Sitz", "Stand", 0),
-                "ganze Stand" to arrayOf(0.2, "Sitz", "Stand", 1),
-                "Sitz" to arrayOf(0, "Sitz", "Sitz", 0),
-                "halbe Sitz" to arrayOf(0.1, "Sitz", "Sitz", 0),
-                "ganze Sitz" to arrayOf(0.2, "Sitz", "Sitz", 0),
-                "Rücken" to arrayOf(0.1, "Sitz", "Rücken", 1),
-                "Halbe Rücken" to arrayOf(0.2, "Sitz", "Rücken", 1),
-                "Bauch" to arrayOf(0.1, "Sitz", "Bauch", 0)
-        )
-        var elementeAusRücken :  Map<String,Array<Any>> = mapOf(
-                "Stand" to arrayOf(0.1, "Rücken", "Stand", 0),
-                "halbe Stand" to arrayOf(0.2, "Rücken", "Stand", 0),
-                "ganze Stand" to arrayOf(0.3, "Rücken", "Stand", 1),
-                "Muffel" to arrayOf(0.3, "Rücken", "Stand", 0),
-                "Salto vorwärts z. Stand" to arrayOf(0.6, "Rücken", "Stand", 1),
-                "halbe Sitz" to arrayOf(0.2, "Rücken", "Sitz", 1),
-                "Muffel in denSitz" to arrayOf(0.3, "Rücken", "Sitz", 0),
-                "Rücken" to arrayOf(0, "Rücken", "Rücken", 0),
-                "Halbe Rücken" to arrayOf(0.3, "Rücken", "Rücken", 0),
-                "Salto vorwärts z. Rücken c" to arrayOf(0.5, "Rücken", "Rücken", 0),
-                "Muffel in den Rücken" to arrayOf(0.5, "Rücken", "Stand", 1),
-                "Bauch" to arrayOf(0.2, "Rücken", "Bauch", 1),
-                "Muffel in den Bauch" to arrayOf(0.2, "Rücken", "Bauch", 1),
-                "Wende in den Bauch" to arrayOf(0.2, "Rücken", "Bauch", 0)
-        )
-        var elementeAusBauch :  Map<String,Array<Any>> = mapOf(
-                "Stand" to arrayOf(0.1, "Bauch", "Stand", 0),
-                "halbe Stand" to arrayOf(0.1, "Bauch", "Stand", 0),
-                "Rücken" to arrayOf(0.2, "Bauch", "Rücken", 0),
-                "Wende in den Rücken" to arrayOf(0.2, "Bauch", "Rücken", 0),
-                "Bauch" to arrayOf(0, "Bauch", "Bauch", 0),
-                "halbe Heli" to arrayOf(0.1, "Bauch", "Bauch", 0)
-        )
         //fun Random.asJavaRandom(): Random
-        val random = Random()
-        fun rand(from: Int, to: Int): Int {
-            return random.nextInt(to - from) + from
-        }
-        val randomElementNumber = Random().nextInt(elementeAusBauch.size) + 1
-        println(elementeAusBauch[randomElementNumber])
+
+        //var erstesElement : Array<Any>? = elementeAusStand[randomNumber]
+        //open val keys: elementeAusStand <K>
+        //val randomElementNumber = Random().nextInt(elementeAusBauch.size) + 1
+        //println(elementeAusBauch[randomElementNumber])
         //println(elementeAusBauch[random.nextInt(elementeAusBauch.size)])
 
 
-
-
-            //val elementeInStand : HashMap<String,Array<Integer,String,String,Integer>> = hashMapOf<String,Array<Int,String,String,Int>()
-        //val elementeAusStand: listOf<Object>()
-        //dyn.whatever()
-        //val elementeAusStand = Arraylist()
 
         btn_jump_to_user_settings.setOnClickListener {
             val intent = Intent(this, UserSettings::class.java)
@@ -119,17 +117,65 @@ class MainScreen : AppCompatActivity(), NavigationView.OnNavigationItemSelectedL
         }
 
         btn_new_exercise.setOnClickListener {
-            val greeting = findViewById<View>(R.id.greeting)
-            greeting.visibility = View.INVISIBLE
+
+            //greeting.visibility = View.INVISIBLE
             list_elemente_der_uebung.visibility = View.VISIBLE
+            var newExercise = generateNewExercise()
+
+            for (element in newExercise){
+
+                //setContentView(R.layout.content_main_screen)
+                val greeting = findViewById<View>(R.id.greeting)
+                //val greeting = findViewById<View>(R.id.greeting)
+                //TextView greeting = (TextView) findViewById(R.id.greeting)
+                //greeting.text.setText(uebung)
+                //TextView exercise = findViewById(R.id.exercise)
+                exercise.setText(element.toString())
+            }
 
         }
 
-
-
         }
 
-    fun generateNewExercise() {
+    fun getNewElement(startingPosition : String):Pair<String,Array<Any>>{
+        var keysOfDict = emptySet<String>()
+        when (startingPosition){
+            "Stand" -> keysOfDict = elementeAusStand.keys
+            "Sitz" -> keysOfDict = elementeAusSitz.keys
+            "Rücken" -> keysOfDict = elementeAusRuecken.keys
+            "Bauch" -> keysOfDict = elementeAusBauch.keys
+            else ->listOf("")
+        }
+
+        var DictList = elementeAusStand.map{it.key to it.value}.shuffled()
+        var newElement = DictList[0]
+
+        var randomNumber = rand(0,elementeAusStand.size)
+
+        //var newElement = elementeAusStand.keys.get(randomNumber: Int)
+        //val keysOfDictTwo=listOf("A","B")
+        //var newElement = keysOfDict.get(0)//(randomNumber)
+
+        return newElement
+
+    }
+    fun generateNewExercise():MutableList<Pair<String,Array<Any>>> {
+        var jumpsInExercise = 0
+        var newExercise : MutableList<Pair<String,Array<Any>>> = mutableListOf()
+        while (jumpsInExercise<10){
+            var newElement : Pair<String,Array<Any>> = "Hocke" to arrayOf(0, "Stand", "Stand", 0)
+            if (newExercise.size==0){
+                newElement = getNewElement("Stand")
+            }
+            else if (newExercise.size>0) {
+                var letztesElement=newExercise[newExercise.lastIndex]
+                newElement = getNewElement(letztesElement.second[2].toString()) //HIER DIE POSITION NACH DEM LETZTEN ELEMENT HOLEN
+            }
+            newExercise.add(newElement)
+            jumpsInExercise+=1
+        }
+
+        return newExercise
 
     }
 
